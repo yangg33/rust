ba25b0d85adfda164500f769671f820fc6a7da06
yangg33 <117976468+yangg33@users.noreply.github.com>
2024-05-24 21:31:37 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let bg = document.querySelector('.mouse-parallax-bg');
+window.addEventListener('mousemove', function(e) {
+    let x = e.clientX / window.innerWidth;
+    let y = e.clientY / window.innerHeight;
+    bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
+});
